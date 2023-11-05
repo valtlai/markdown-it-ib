84722b222c2dfd01e354915d037a44aab352e3bf
Valtteri Laitinen <dev@valtlai.fi>
2023-11-05 05:46:46 +0200

    Name exported fuction

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 function render(tokens, idx, options, _env, self) {
 	const token = tokens[idx];
-	if (token.markup === '*') token.tag = 'i';
-	else if (token.markup === '**') token.tag = 'b';
+	if (token.markup === '*') {
+		token.tag = 'i';
+	} else if (token.markup === '**') {
+		token.tag = 'b';
+	}
 	return self.renderToken(tokens, idx, options);
 }
 
-module.exports = function (md) {
+module.exports = function markdownItIB(md) {
 	md.renderer.rules.em_open = render;
 	md.renderer.rules.em_close = render;
 	md.renderer.rules.strong_open = render;
